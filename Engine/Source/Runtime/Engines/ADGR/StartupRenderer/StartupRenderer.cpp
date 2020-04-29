969dd433a037c026a66541f1db9025d6539bcc9f
Dhiraj Wishal <wishaldhiraj@gmail.com>
2020-04-29 10:20:51 +0530

    Adding new Icon for the Dynamik Engine.

--- Engine/Source/Runtime/Engines/ADGR/StartupRenderer/StartupRenderer.cpp
+++ Engine/Source/Runtime/Engines/ADGR/StartupRenderer/StartupRenderer.cpp
@@ -126,7 +126,7 @@ namespace Dynamik {
 			UCHR* data = stbi_load(path, &width, &height, &nrChannels, 0);
 			if (data)
 			{
-				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
 				glGenerateMipmap(GL_TEXTURE_2D);
 			}
 			else
